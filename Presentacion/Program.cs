2e66042a04c24f10cc90d7f8c89af4237547b86f
Alumno <Alumno@LAB1504-25>
2023-10-13 21:19:57 -0500

    Agregar archivos de proyecto.

---
+++ Presentacion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Entity;
+
+namespace Presentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el nombre del producto a buscar: ");
+            string productName = Console.ReadLine();
+
+            ProductDataAccess productDataAccess = new ProductDataAccess();
+            List<Product> products = productDataAccess.GetProductsByName(productName);
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"ID: {product.ProductId}, Nombre: {product.Name}, Precio: {product.Price:C}, Stock: {product.Stock}");
+            }
+        }
+    }
+}
